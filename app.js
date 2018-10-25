e8c45ad5f3a58f540e9ab3ed5ca3a36b9b972fc9
bgabot89 <bgabot89@gmail.com>
2018-10-25 16:51:36 -0700

    changing syntax to allow heroku to dynamically set port

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ app.post('/charge', function(req,res) {
   // res.send('TEST');
 });
 
-
-app.listen('3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log('working on 3000');
